f246fca4cb1e9784941781c484e4514b78e3eb66
Documenter.jl <documenter@juliadocs.github.io>
2022-09-05 07:11:14 +0000

    build based on b72361b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OmicsProfiles","category":"page"},{"location":"#OmicsProfiles","page":"Home","title":"OmicsProfiles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OmicsProfiles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OmicsProfiles]","category":"page"}]
+}
