8fa96c5a8fe7cf40141cc9d977a6104cd476bee2
Yueh-Hua Tu <a504082002@gmail.com>
2023-02-19 21:20:42 +0800

    Support reading distinct name of feature files

--- test/io.jl
+++ test/io.jl
@@ -1,12 +1,14 @@
 @testset "io" begin
     data_path = joinpath(TEST_PATH, "data")
     mtx_filename = joinpath(data_path, "test.mtx")
-    genes1 = OmicsProfiles.read_genes(joinpath(data_path, "genes.tsv"))
-    cells1 = OmicsProfiles.read_cells(joinpath(data_path, "cells.tsv"))
+    genes1 = OmicsProfiles.read_csv(joinpath(data_path, "genes.tsv"),
+                                    header=OmicsProfiles.HCA_GENES_HEADER)
+    cells1 = OmicsProfiles.read_csv(joinpath(data_path, "cells.tsv"))
     mtx = OmicsProfiles.read_mtx(mtx_filename)
 
-    genes2 = OmicsProfiles.read_genes(joinpath(data_path, "genes.tsv.gz"))
-    cells2 = OmicsProfiles.read_cells(joinpath(data_path, "cells.tsv.gz"))
+    genes2 = OmicsProfiles.read_csv(joinpath(data_path, "genes.tsv.gz"),
+                                    header=OmicsProfiles.HCA_GENES_HEADER)
+    cells2 = OmicsProfiles.read_csv(joinpath(data_path, "cells.tsv.gz"))
 
     @test genes1 == genes2
     @test cells1 == cells2
@@ -16,7 +18,7 @@
         [1, 1, 1, 1, 1],
         11, 12
     )
-
+    
     pbmc3k_path = SnowyOwl.Dataset.pbmc3k_folder()
     prof = OmicsProfiles.read_10x(pbmc3k_path, varindex=:ensembleid)
 end
